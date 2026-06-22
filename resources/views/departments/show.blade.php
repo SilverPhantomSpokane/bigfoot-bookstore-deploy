274c6e93eb87846fd628fd9285a079d8ed4ec92a
Mariia Baranova <mariiabaranova@Mariias-MacBook-Air.local>
2026-06-21 18:01:20 -0700

    Show full book covers

--- resources/views/departments/show.blade.php
+++ resources/views/departments/show.blade.php
@@ -19,7 +19,7 @@
             {{-- image --}}
             @if($product->image)
                 <img src="{{ asset($product->image) }}" 
-                     class="w-full h-48 object-cover rounded mb-2" 
+                     class="w-full h-64 object-contain rounded mb-2 bg-gray-50"
                      alt="{{ $product->name }}">
             @else
                 <div class="w-full h-48 bg-gray-200 flex items-center justify-center text-gray-500 rounded mb-2">
